57851fcc9351777f00c5f985f4be5b950277649c
Djangø <63513594+Djngo@users.noreply.github.com>
2020-04-13 15:26:40 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,7 @@
-(your ndk path here)\ndk-build
- NDK_PROJECT_PATH=(your project path here)
- NDK_APPLICATION_MK=\jni\Application.mk
+@echo off
+cd (PUT YOUR NDK PATH HERE)
+call ndk-build NDK_PROJECT_PATH=(PUT YOUR PROJECT PATH HERE) NDK_APPLICATION_MK=(PUT YOUR PROJECT PATH HERE)\jni\Application.mk
+
+if "%errorlevel%"=="0" echo Build successful.
+
+pause
