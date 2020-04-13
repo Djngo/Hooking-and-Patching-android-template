04ff1ce6faffb2db2476eadcae4f8b7c0325ab37
Djangø <63513594+Djngo@users.noreply.github.com>
2020-04-13 14:51:17 +0200

    Structure changes & fixes

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,18 +3,19 @@ MAIN_LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := yeet
 
-LOCAL_CFLAGS := -Wno-error=format-security -fpermissive
+LOCAL_CPPFLAGS := -std=c++11
+LOCAL_CFLAGS := -Wno-error=format-security -fpermissive -Wc++11-extensions -Wno-absolute-value
 LOCAL_CFLAGS += -fno-rtti -fno-exceptions
 
 LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)
 
-LOCAL_SRC_FILES := main.cpp \
-				   Substrate/hde64.c \
-                   Substrate/SubstrateDebug.cpp \
-                   Substrate/SubstrateHook.cpp \
-                   Substrate/SubstratePosixMemory.cpp \
-				   KittyMemory/KittyMemory.cpp \
-				   KittyMemory/MemoryPatch.cpp \
+LOCAL_SRC_FILES := src/main.cpp \
+				   libs/Substrate/hde64.c \
+                   libs/Substrate/SubstrateDebug.cpp \
+                   libs/Substrate/SubstrateHook.cpp \
+                   libs/Substrate/SubstratePosixMemory.cpp \
+				   libs/KittyMemory/KittyMemory.cpp \
+				   libs/KittyMemory/MemoryPatch.cpp \
 
 LOCAL_LDLIBS := -llog
 
